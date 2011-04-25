878e0db3ad2a6dc008e11094c73168f6d7a0fa1d
Andrew Nesbitt <andrewnez@gmail.com>
2011-04-25 17:30:37 +0100

    Send email with pony via sendgrid on heroku

--- contact.rb
+++ contact.rb
@@ -9,7 +9,22 @@ module Nesta
     end
 
     post '/' do
-      # send email with pony
+      Pony.mail(
+        :from => params[:name] + "<" + params[:email] + ">",
+        :to => 'andrewnez@gmail.com, banesbitt@dsl.pipex.com',
+        :subject => "Bude Chalet contact enquiry",
+        :body => params[:message] + "\n\nTelephone: " + params[:telephone] + "\n\nEmail: " + params[:email],
+        :port => '587',
+        :via => :smtp,
+        :via_options => {
+          :address              => 'smtp.sendgrid.net',
+          :port                 => '587',
+          :enable_starttls_auto => true,
+          :user_name            => ENV['SENDGRID_USERNAME'],
+          :password             => ENV['SENDGRID_PASSWORD'],
+          :authentication       => :plain,
+          :domain               => ENV['SENDGRID_DOMAIN']
+        })
       # set success message
       haml(:contact)
     end
