496fd24900459386478d6cd2fc72fcd85c1258da
Andrew Nesbitt <andrewnez@gmail.com>
2011-04-25 17:09:05 +0100

    Postable contact form

--- contact.rb
+++ contact.rb
@@ -1,13 +1,17 @@
 module Nesta
   class Contact < Nesta::App
-    get '/' do
+    before do
       set_common_variables
+    end
+
+    get '/' do
       cache haml(:contact)
     end
 
     post '/' do
       # send email with pony
       # set success message
+      haml(:contact)
     end
   end
 end
